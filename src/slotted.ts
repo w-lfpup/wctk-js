7d3d13c8cc159f7f51ad486ef13adc859da89ec0
Taylor Vann <brian.t.vann@gmail.com>
2025-06-03 13:04:32 -0700

    remove event listeners

--- src/slotted.ts
+++ src/slotted.ts
@@ -3,41 +3,23 @@ interface SlottedParamsInterface {
 	connected: boolean;
 }
 
-interface SlottedInterface {}
-
 interface NewAble<A> {
 	new (): A;
 }
 
+interface SlottedInterface {
+	query(): void;
+	assignedNodes<A>(slotName: string, newable: NewAble<A>): Node[] | undefined,
+	assignedElements(slotName: string, selectors: string): Element[] | undefined,
+}
+
 class Slotted implements SlottedInterface {
 	#params: SlottedParamsInterface;
-	#boundOnSlotChange: EventListenerOrEventListenerObject;
 	#slots: Map<string, HTMLSlotElement>;
 
 	constructor(params: SlottedParamsInterface) {
 		this.#params = params;
-		this.#boundOnSlotChange = this.#onSlotChange.bind(this);
 		this.#slots = getSlotElements(this.#params.target);
-
-		if (this.#params.connected) this.connect();
-	}
-
-	#onSlotChange(e: Event) {
-		let { target } = e;
-		if (target instanceof HTMLSlotElement) {
-			this.#slots.set(target.name, target);
-		}
-	}
-
-	connect() {
-		this.#params.target.addEventListener("slotchange", this.#boundOnSlotChange);
-	}
-
-	disconnect() {
-		this.#params.target.removeEventListener(
-			"slotchange",
-			this.#boundOnSlotChange,
-		);
 	}
 
 	query() {
@@ -52,25 +34,25 @@ class Slotted implements SlottedInterface {
 		return this.#slots.get(slotName)?.assignedElements();
 	}
 
-	assignedInstances<A>(slotName: string, instance: NewAble<A>) {
+	assignedInstances<A>(slotName: string, newable: NewAble<A>) {
 		let slot = this.#slots.get(slotName);
 
 		let instances = [];
 		if (slot)
 			for (const node of slot.assignedNodes()) {
-				if (node instanceof instance) instances.push(node);
+				if (node instanceof newable) instances.push(node);
 			}
 
 		return instances;
 	}
 
-	assignedMatches(slotName: string, selector: string): Element[] {
+	assignedMatches(slotName: string, selectors: string): Element[] {
 		let slot = this.#slots.get(slotName);
 
 		let matches = [];
 		if (slot)
 			for (const element of slot.assignedElements()) {
-				if (element.matches(selector)) matches.push(element);
+				if (element.matches(selectors)) matches.push(element);
 			}
 
 		return matches;
