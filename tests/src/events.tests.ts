8183db6bd860f368509dda906e229ec34e6ee79e
Taylor Vann <brian.t.vann@gmail.com>
2026-05-17 16:04:55 -0700

    add event tests

--- tests/src/events.tests.ts
+++ tests/src/events.tests.ts
@@ -1,10 +1,76 @@
-import { findElement } from "@w-lfpup/jackrabbit/browser/dist/commands.js";
+import {
+	findElement,
+	elementClick,
+	elementSendKeys,
+} from "@w-lfpup/jackrabbit/browser/dist/commands.js";
+import { Events } from "../../dist/mod.js";
 
-function lilTest() {
-	return;
+let eventController: Events;
+let eventReceipts: Event[] = [];
+
+function setup() {
+	eventController = new Events({
+		target: document.body,
+		connected: true,
+		listeners: {
+			click: function (e: PointerEvent) {
+				eventReceipts.push(e);
+			},
+			input: function (e: InputEvent) {
+				eventReceipts.push(e);
+			},
+		},
+	});
+
+	document.body.setHTMLUnsafe(`
+		<input>
+		<button>
+	`);
 }
 
-export const tests = [lilTest];
+async function testClickEvents() {
+	// click button
+	// click button again
+	let buttonId = await findElement("button");
+	if (!buttonId) return "failed to query button";
+	await elementClick(buttonId);
+	await elementClick(buttonId);
+
+	let clicks = [];
+	for (let event of eventReceipts) {
+		if ("click" === event.type) clicks.push(event);
+	}
+
+	if (2 !== clicks.length)
+		return `incorrect number of clicks: ${clicks.length}/2`;
+}
+
+async function testInputEvents() {
+	// query input
+	// send keys to input
+	const expectedMessage = "UwU";
+	let inputId = await findElement("input");
+	if (!inputId) return "failed to query input element";
+
+	await elementSendKeys(inputId, expectedMessage);
+	let inputs = [];
+	for (let event of eventReceipts) {
+		if ("input" === event.type) inputs.push(event);
+	}
+
+	if (3 !== inputs.length)
+		return `incorrect number of input events: ${inputs.length}/1`;
+
+	let input = document.body.querySelector("input");
+	if (expectedMessage !== input?.value)
+		return `expected input: ${expectedMessage}, found: ${input?.value}`;
+}
+
+function tearDown() {
+	eventController.disconnect();
+}
+
+export const tests = [setup, testClickEvents, testInputEvents, tearDown];
 
 export const options = {
 	title: import.meta.url,
